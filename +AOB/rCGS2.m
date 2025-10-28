76cf5d2483d0bf9f64173384ac3615f4e09fba5a
yuekai-yan <yyk20001219@163.com>
2025-10-28 11:03:36 +0100

    10.28

---
+++ +AOB/rCGS2.m
@@ -0,0 +1,29 @@
+function [Q, R] = rCGS2(X, B, Theta)
+    %Randomized classic Gram-Schmidt with reorthogonalization 
+    %against other blocks
+    %Input:
+    %X      n-by-m matrix, whose columns are Theta
+    %       orthonormal
+    %B      small block matrix of size n-by-s to be 
+    %       Theta orthogonalized with X
+    %Theta  d-by-n matrix
+    %Output:
+    %Q      Theta orthogonalized matrix against X of size n-by-s
+    %R      m-by-s matrix
+
+    n = size(X, 1);
+    m = size(X, 2);
+    s = size(B, 2);
+    Q = zeros(n, s);
+    R = zeros(m, s);
+
+    P = Theta * B;  %d-by-s matrix
+    S = Theta * X;  %d-by-m matrix
+    R0 = S' * P;  %m-by-s matrix
+    Q0 = B - X * R0;
+    %reorthogonalization
+    S0 = Theta * Q0;
+    R1 = S' * S0;
+    R = R0 + R1;
+    Q = Q0 - X * R1;
+end
--- +WB/CGS2.m
+++ +WB/CGS2.m
@@ -1,32 +1,25 @@
-function [Q, R] = rCGS2(X, Theta)
-    %Randomized Classic Gram-Schmidt with reorthogonalization 
+function [Q, R] = CGS2(X)
+    %Classic Gram-Schmidt with reorthogonalization 
     %within a block
     %Input: 
     %X n-by-m matrix to be orthogonalized
-    %Theta random sketch of size (d, n)
     %Output: 
     %Q n-by-m matrix, column-oriented orthonormal matrix
     %R m-by-m matrix, coefficients of the basis spanned by Q
 
     n = size(X, 1);
     m = size(X, 2);
-    d = size(Theta, 1);
     Q = zeros(n, m);
-    S = zeros(d, m);
     R = zeros(m, m);
-    P = Theta * X;  %d-by-m matrix
     for i = 1:m
-        w0 = S(:, 1:i-1)' * P(:, i);  %(i-1)-by-1 vector
+        w0 = Q(:, 1:i-1)' * X(:, i);  %(i-1)-by-1 vector
         q0 = X(:, i) - Q(:, 1:i-1) * w0;
-        s0 = Theta * q0;
         %reorthogonalization
-        w = S(:, 1:i-1)' * s0;
+        w = Q(:, 1:i-1)' * q0;
         R(1:i-1, i) = w0 + w;
         q = q0 - Q(:, 1:i-1) * w;
 
-        s = Theta * q;
-        R(i, i) = norm(s);
-        S(:, i) = s / R(i, i);
+        R(i, i) = norm(q);
         Q(:, i) = q / R(i, i);
     end
 end
